c070dc3caf0071a8dc96f5936734f618782fe60a
Nicolas Cueva <nicolascueva@outlook.com>
2018-08-06 21:54:44 -0500

    Finished HTML/CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//var computerScore = random;
+var userScore = 0;
+var wins = 0;
+var losses = 0;
+//var redGemValue = random;
+//var blueGemValue = random;
+//var yellowGemValue = random;
+//var greenGemValue = random;
+
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+});
