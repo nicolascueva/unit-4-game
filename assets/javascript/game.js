01f13ad9f838abe3302d79f882bfb45a720971f6
Nicolas Cueva <nicolascueva@outlook.com>
2018-08-06 22:58:51 -0500

    Added half of JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,35 @@
-//var computerScore = random;
+var computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 var userScore = 0;
 var wins = 0;
 var losses = 0;
-//var redGemValue = random;
-//var blueGemValue = random;
-//var yellowGemValue = random;
-//var greenGemValue = random;
+var redGemValue = Math.floor(Math.random() * 12) + 1;
+var blueGemValue = Math.floor(Math.random() * 12) + 1;
+var yellowGemValue = Math.floor(Math.random() * 12) + 1;
+var greenGemValue = Math.floor(Math.random() * 12) + 1;
 
 
 $(document).ready(function () {
+    $("#computerScore").append(computerScore);
 
+    $("#redGemValue").click(function () {
+        console.log(redGemValue);
+
+    });
+
+    $("#blueGemValue").click(function () {
+        console.log(blueGemValue);
+
+    });
+
+    $("#yellowGemValue").click(function () {
+        console.log(yellowGemValue);
+
+    });
+
+    $("#greenGemValue").click(function () {
+        console.log(greenGemValue);
+
+    });
 
 
 
