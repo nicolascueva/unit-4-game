f4cab7dc33bdee5bd3c6845fb871d0c0476feb30
Nicolas Cueva <nicolascueva@outlook.com>
2018-08-08 13:04:39 -0500

    Almost Finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,36 +6,117 @@ var redGemValue = Math.floor(Math.random() * 12) + 1;
 var blueGemValue = Math.floor(Math.random() * 12) + 1;
 var yellowGemValue = Math.floor(Math.random() * 12) + 1;
 var greenGemValue = Math.floor(Math.random() * 12) + 1;
+var userTotal = userScore;
 
 
 $(document).ready(function () {
-    $("#computerScore").append(computerScore);
 
+    $("#computerScore").html(computerScore);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    $("#userScore").html(userScore);
+
+    function gameWin() {
+        //alert("You win the game!");
+        wins++;
+        $("#wins").text(wins);
+        reset();
+    }
+
+    function gameLose() {
+        // alert("Loser!");
+        losses++;
+        $("#losses").text(losses);
+        reset();
+    }
+
+    function reset() {
+        computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        //console.log(Random)
+        $("#computerScore").html(computerScore);
+        redGemValue = Math.floor(Math.random() * 12) + 1;
+        blueGemValue = Math.floor(Math.random() * 12) + 1;
+        yellowGemValue = Math.floor(Math.random() * 12) + 1;
+        greenGemValue = Math.floor(Math.random() * 12) + 1;
+        userScore = 0;
+        $("#userScore").html(userScore);
+    }
+
+
+
+    ///////////////// gem values
     $("#redGemValue").click(function () {
-        console.log(redGemValue);
+        userScore = userScore + redGemValue;
+        $("#userScore").html(userScore);
 
+
+        if (userScore === computerScore) {
+            console.log("you win");
+            gameWin();
+        }
+        else if (userScore > computerScore) {
+            console.log("you lose");
+            gameLose();
+        }
     });
 
     $("#blueGemValue").click(function () {
-        console.log(blueGemValue);
+        userScore = userScore + blueGemValue;
+        $("#userScore").html(userScore);
+
+
+        if (userScore === computerScore) {
+            console.log("you win");
+            gameWin();
+        }
+        else if (userScore > computerScore) {
+            console.log("you lose");
+            gameLose();
+        }
 
     });
 
     $("#yellowGemValue").click(function () {
-        console.log(yellowGemValue);
+        userScore = userScore + yellowGemValue;
+        $("#userScore").html(userScore);
+
+        if (userScore === computerScore) {
+            console.log("you win");
+            gameWin();
+        }
+        else if (userScore > computerScore) {
+            console.log("you lose");
+            gameLose();
+        }
 
     });
 
     $("#greenGemValue").click(function () {
-        console.log(greenGemValue);
+        userScore = userScore + greenGemValue;
+        $("#userScore").html(userScore);
+
+        if (userScore === computerScore) {
+            console.log("you win");
+            gameWin();
+        }
+        else if (userScore > computerScore) {
+            console.log("you lose");
+            gameLose();
+        }
 
     });
+    ////////////////gem values
 
 
-
-
-
-
+    ////// can this outside work?????
+    /*if (userTotal === computerScore) {
+        console.log("you win");
+        wins++;
+    }
+    if (userTotal > computerScore) {
+        console.log("you lose");
+        losses++;
+    }*/
 
 
 });
